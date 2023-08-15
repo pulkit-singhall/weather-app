af436f36a212e885f020f5a87482666edf5b3693
pulkit-singhall <singhal.pulkit667@gmail.com>
2023-08-15 23:48:08 +0530

    Preference

---
+++ lib/preference.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Preference extends StatelessWidget {
+  const Preference({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
